46a8eaebe7f32546515b237acf48fec9a0fee531
first commit <manirock123w@gmail.com>
2021-12-08 12:16:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,153 @@
+// console.log("Hi Manikanta");
+// age=20;
+// name="manikanta";
+// console.log(age);
+// console.log(name);
+// console.log("my name is " +name+ " my age is " +age);
+// console.log(`my name is ${name} my age is ${age}`);
+
+// const s='Hello World';
+// console.log(s.substring(0, 9).toUpperCase());
+// console.log(s.split(''))
+// const s = "technology, computers, it, code";
+// console.log(s.split(', '));
+
+ //const fruits=['apples', 'oranges', 'pears'];
+ //const fruits = 10;
+// fruits[3]='grapes';
+// fruits.push('mangoes');
+// fruits.unshift('strawberries');
+// fruits.pop();
+// console.log(Array.isArray(fruits));
+// console.log(fruits);
+
+    //     const person = {
+    //     firstName:'mani',
+    //     lastName:'kanta', 
+    //     age:'20',
+    //     hobbies:['movies','games'],
+    //     address: {
+    //          village:'saripalli',
+    //          city:'vizag',
+    //          state:'AP'
+    //      }
+    //  }
+    // console.log(person.address.city);
+
+// var persons =["mani","vasu","atchibabu"];
+// console.log(persons[2]);
+// persons[1]='shanmukh';
+// console.log(persons);
+// persons[3]='sai';
+// console.log(persons);
+
+
+// const person={
+//     name:'mani',
+//     age:20
+// }
+
+// console.log(person);
+// console.log(person.age);
+// console.log(person.name);
+// person.name='manikanta';
+// console.log(person);
+
+// object
+// const todos=[
+//     {
+//     id: 1,
+//     text: 'take out trash',
+//     iscompleted: true
+//     },
+//     {
+//     id: 2,
+//     text: 'meeting with boss',
+//     iscompleted: true
+//     },
+//         {
+//     id: 3,
+//     text: 'dentist appt',
+//     iscompleted: false
+//     },    
+// ]
+
+// const todoJSON=JSON.stringify(todos);
+// console.log(todoJSON);
+
+// array
+//   var persons =["mani","vasu","atchibabu","harry"];
+//   for(var i=0; i<persons.length; i++){
+//      console.log(persons[i]);
+//   }
+
+// for loop
+// i=1;
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+// while loop
+// let i=1;
+// while(i<10){
+//     console.log(`while loop number ${i}`);
+//     i++;
+// }
+
+// if else
+// let x = 10;
+// if(x === 10){
+//     console.log('x is 10');
+// }else if(x>10){
+//     console.log('x is greater than 10');
+// }else {
+//     console.log('x is lessthan 10');
+// }
+
+// let x=4;
+// let y=9;
+// if(x>3  y>8){
+//         console.log('x is more than 7 and y is more than 10');
+// }
+
+// switch
+// const x=9;
+// const color= x>9 ? 'red' : 'blue';
+// console.log(color);
+
+// switch (color) {
+//     case 'red':
+//         console.log('color is red');
+//         break;
+//     case 'blue':
+//         console.log('color is blue');
+//         break; 
+//     default:
+//         console.log('color is not');
+//         break;
+// }
+ 
+// functions
+// function addnums (num1,num2) {
+//     console.log(num1 + num2);
+// }
+// addnums('mani','kanta');
+
+// function Person(firstName,lastName,dob) {
+//     this.firstName=firstName;
+//     this.lastName=lastName;
+//     this.dob=new Date(dob);
+//     this.getFullName=function(){
+//         return `${this.firstName}  ${this.lastName}`;
+//     }
+// }
+
+// const person1=new Person('mani','kanta','7-3-2001');
+// const person2=new Person('atchi','babu','1-1-1980');
+
+// console.log(person1.dob.getFullYear());    
+// console.log(person1.getFullName());  
+
+
+
+
